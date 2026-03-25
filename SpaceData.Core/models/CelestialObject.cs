9afa6365571bd2d9a9a4da3fd703739a2cf4eb68
uneverdreamed <ma2005f@gmail.com>
2026-03-25 11:25:31 +0300

    SpaceData.Core | models | создан базовый абстрактный класс CelestialObject для всех космических объектов

---
+++ SpaceData.Core/models/CelestialObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceData.Core.models
+{
+    public abstract class CelestialObject // базовый класс для всех космических объектов
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; protected set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime LastUpdated { get; set; }
+        protected CelestialObject()
+        {
+            LastUpdated = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $"{Type}: {Name}";
+        }
+
+    }
+}
