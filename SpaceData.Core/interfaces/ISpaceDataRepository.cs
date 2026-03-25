923984723de44fdac5e58c4762a8c07366045020
uneverdreamed <ma2005f@gmail.com>
2026-03-25 11:43:04 +0300

    SpaceData.Core | interfaces | создан интерфейс репозитория ISpaceDataRepository.cs для доступа к данным о космических объектах

---
+++ SpaceData.Core/interfaces/ISpaceDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceData.Core.models;
+
+namespace SpaceData.Core.interfaces
+{
+    public interface ISpaceDataRepository // интерфейс репозитория для доступа к данным о космических объектах
+    {
+        Planet? GetPlanet(string name);
+        Star? GetStar(string name);
+        Galaxy? GetGalaxy(string name);
+        IEnumerable<string> GetAvailablePlanets();
+        IEnumerable<string> GetAvailableStars();
+        IEnumerable<string> GetAvailableGalaxies();
+    }
+}
